Hecked-Stuffs/settings

Settings: Randomize DerpSpace's summary
Thanks to our dear maintainers for helping make that list. Especially marshmello61
Also a big thank you to fortune (https://en.wikipedia.org/wiki/Fortune_(Unix)) for some of them.

Change-Id: Ice2e6c36aa900eac7a0097e96d04a77ebdf4a1d8
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -51,11 +51,16 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements SplitLayoutListener,
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+    private static final String KEY_DERPSPACE = "top_level_derpspace";
     private static final String SAVED_HIGHLIGHT_MIXIN = "highlight_mixin";
     private static final String PREF_KEY_SUPPORT = "top_level_support";
 
@@ -92,6 +97,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         super.onAttach(context);
         HighlightableMenu.fromXml(context, getPreferenceScreenResId());
         use(SupportPreferenceController.class).setActivity(getActivity());
+        updateDerpSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateDerpSummary();
     }
 
     @Override
@@ -348,6 +360,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private interface PreferenceJob {
         default void init() {}
         void doForEach(Preference preference);
+    }
+
+    private void updateDerpSummary() {
+        Preference derpspace = findPreference(KEY_DERPSPACE);
+        if (derpspace != null) {
+            String[] summaries = getContext().getResources().getStringArray(
+                    R.array.derpspace_summaries);
+            Random rnd = new Random();
+            int summNO = rnd.nextInt(summaries.length);
+            derpspace.setSummary(summaries[summNO]);
+        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =

DerpFest-LOS/android_packages_apps_Settings

Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -57,11 +57,16 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.drawer.Tile;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @SearchIndexable(forTarget = MOBILE)
 public class TopLevelSettings extends DashboardFragment implements SplitLayoutListener,
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private static final String TAG = "TopLevelSettings";
+    private static final String KEY_DERPFEST = "top_level_derpfest";
     private static final String SAVED_HIGHLIGHT_MIXIN = "highlight_mixin";
     private static final String PREF_KEY_SUPPORT = "top_level_support";
 
@@ -106,6 +111,13 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         super.onAttach(context);
         HighlightableMenu.fromXml(context, getPreferenceScreenResId());
         use(SupportPreferenceController.class).setActivity(getActivity());
+        updateDerpSummary();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateDerpSummary();
     }
 
     @Override
@@ -390,6 +402,17 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         }
 
         void doForEach(Preference preference);
+    }
+
+    private void updateDerpSummary() {
+        Preference derpfest = findPreference(KEY_DERPFEST);
+        if (derpfest != null) {
+            String[] summaries = getContext().getResources().getStringArray(
+                    R.array.derpfest_summaries);
+            Random rnd = new Random();
+            int summNO = rnd.nextInt(summaries.length);
+            derpfest.setSummary(summaries[summNO]);
+        }
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =